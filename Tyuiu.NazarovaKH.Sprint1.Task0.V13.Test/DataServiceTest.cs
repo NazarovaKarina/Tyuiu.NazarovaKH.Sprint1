f153b06562b34d44dc2b4d6fdda907041428b20a
Tyuiu.NazarovaKKH@outlook.com <NazarovaKH@std.tyuiu.ru>
2024-09-24 21:53:30 +0300

    Add project files.

---
+++ Tyuiu.NazarovaKH.Sprint1.Task0.V13.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.NazarovaKH.Sprint1.Task0.V13.Lib;
+
+namespace Tyuiu.NazarovaKH.Sprint1.Task0.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
